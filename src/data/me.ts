2bb4c4be81ab683ea0885ce46a4d6809b9d510b7
lazosee <oseelazaro@gmail.com>
2025-10-27 21:17:52 +0300

    create new about page

---
+++ src/data/me.ts
@@ -0,0 +1,8 @@
+export const about = {
+	name: 'lazaro osee',
+	image: 'https://picsum.photos/seed/1/1200/600',
+	hobbies: ['drawing', 'painting', 'programming', 'designing', 'modelling'],
+	desc: 'An architecture student with a passion for <code class="text-slate-800 dark:text-slate-200 uppercase">research</code>,<code class="text-slate-800 dark:text-slate-200 uppercase">creation</code>, and <code class="text-slate-800 dark:text-slate-200 uppercase">sharing</code> of ideas and projects',
+	description:
+		'An architecture student with a passion for Research, Creation, and Sharing of Ideas and Projects',
+}
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,9 +1,22 @@
 import { TITLE } from '@/data/nav'
 
-export function titleTemplate(title?: string, base: string = TITLE, sep: '|' | '-' = '|') {
+export function titleTemplate(title?: string, base: string = TITLE, sep: '|' | '-' = '|'): string {
 	return title ? `${title} ${sep} ${base}` : base
 }
 
-export function is<T>(t: T, v: any) {
+export function is<T>(t: T, v: any): boolean {
 	return v == t
+}
+
+export function capitalize(str: string): string {
+	return str
+		.split(' ')
+		.map((char) => `${char.at(0)?.toUpperCase() ?? ''}${char.slice(1)}`)
+		.join(' ')
+}
+export function uppercase(str: string): string {
+	return str
+		.split('')
+		.map((char) => char.toUpperCase())
+		.join('')
 }
