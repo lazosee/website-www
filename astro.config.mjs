4ce518fde2d223f0d08c69772d9a129758192e80
lazosee <oseelazaro@gmail.com>
2025-11-20 18:08:07 +0300

    Update astro config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,39 +11,45 @@ import mdx from '@astrojs/mdx'
 
 import sitemap from '@astrojs/sitemap'
 
-import svelte from '@astrojs/svelte';
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    site: 'http://localhost:4321', //'https://www.lazaroosee.xyz',
-    adapter: netlify({
-        edgeMiddleware: true,
-        imageCDN: true,
-        devFeatures: {
-            environmentVariables: true,
-            images: true,
-        },
-    }),
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    integrations: [markdoc({}), react(), mdx({ optimize: true }), sitemap({
-        changefreq: 'weekly',
-        priority: 0.7,
-        entryLimit: 10_000,
-        filter: (page) => !page.includes('/admin'),
-        serialize: (item) => {
-            if (/admin/.test(item.url)) {
-                return undefined
-            }
-            return item
-        },
-		}), svelte()],
-    image: {
-        remotePatterns: [
-            { protocol: 'https', hostname: 'picsum.photos' },
-            { protocol: 'https', hostname: 'placehold.co' },
-        ],
-    },
+	output: 'server',
+	site: 'https://www.lazaroosee.xyz', //'http://localhost:4321',
+	adapter: netlify({
+		edgeMiddleware: true,
+		imageCDN: true,
+		devFeatures: {
+			environmentVariables: true,
+			images: true,
+		},
+	}),
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [
+		markdoc({}),
+		react(),
+		mdx({ optimize: true }),
+		sitemap({
+			changefreq: 'weekly',
+			priority: 0.7,
+			entryLimit: 10_000,
+			filter: (page) => !page.includes('/admin'),
+			serialize: (item) => {
+				if (/admin/.test(item.url)) {
+					return undefined
+				}
+				return item
+			},
+		}),
+		svelte(),
+	],
+	image: {
+		remotePatterns: [
+			{ protocol: 'https', hostname: 'picsum.photos' },
+			{ protocol: 'https', hostname: 'placehold.co' },
+		],
+	},
 })
